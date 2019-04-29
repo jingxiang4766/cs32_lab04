573b387f126e280f19c6ec324da073a374600d96
Jing Su <jingxiang@ucsb.edu>
2019-04-29 12:41:12 -0700

    initial commit

---
+++ table.h
@@ -0,0 +1,12 @@
+//
+//  table.h
+//  S19 - Lab04
+//
+#ifndef table_h
+#define table_h
+
+
+// Note: Your hash table must be implemented using an array of
+// std::vector<Entry> as stated in the instructions.
+
+#endif /* table_h */
