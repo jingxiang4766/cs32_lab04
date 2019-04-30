6d4d3909183ce14f02db6f7845f785ce8ff4503f
Jing Su <jingxiang@ucsb.edu>
2019-04-29 22:46:31 -0700

    stub

---
+++ table.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "table.h"
+#include "entry.h"
+
+Table::Table(unsigned int max_entries):max_entries(max_entries){}
+
+Table::Table(unsigned int entries, std::istream& input){
+	"stub";
+}
+
+void Table::put(unsigned int key, std::string data){
+	"stub";
+}
+
+void Table::put(Entry e){
+	"stub";
+}
+
+std::string Table::get(unsigned int key) const {
+	return "stub";
+}
+
+bool Table::remove(unsigned int key){
+	return false;
+}
+
+std::ostream& operator<< (std::ostream& out, const Table& t){
+	return out;
+}
